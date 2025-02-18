d766c3d1eaff546e33088c2030631d138c8d5211
hossam <105924435+hosssam21@users.noreply.github.com>
2025-02-18 05:40:14 +0200

    Notification Adding

---
+++ Presentation/Notification.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace EntityFramework_Day3_Lab.Presentation
+{
+    partial class Notification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 82);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(776, 356);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(161, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(460, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Tasks Not Completed Yet and Due Date has been lost";
+            // 
+            // Notification
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Notification";
+            Text = "Notification";
+            Load += Notification_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+    }
+}
---
+++ Presentation/Notification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityFramework_Day3_Lab.application.Services;
+using EntityFramework_Day3_Lab.Domain.Model;
+
+namespace EntityFramework_Day3_Lab.Presentation
+{
+    public partial class Notification : Form
+    {
+        int user_id;
+        UserServices UserServices02 = new UserServices();
+
+        public Notification(int Id)
+        {
+            InitializeComponent();
+            user_id = Id;
+
+        }
+
+        private void Notification_Load(object sender, EventArgs e)
+        {
+            var result = UserServices02.GetTasks(user_id).Where(T=>T.DueDate<DateTime.Now && (T.Status ==Status.Pending || T.Status==Status.InProgress)).ToList();
+            dataGridView1.DataSource = result;
+        }
+    }
+}
